b7638f3077fa9f217a14dfd5cd622ebe2067dd3a
Techthoth <ayotomiwadavid@proton.me>
2024-06-06 08:47:34 -0700

    saving progress

--- clientside/src/Components/Loginform.js
+++ clientside/src/Components/Loginform.js
@@ -1,100 +1,107 @@
-import React from 'react'
-import Button from './ui/Button'
-import Input from './forms/Input'
-import Section from './global/Section'
-import Container from './global/Container'
-
+import React, { useRef, useState } from "react";
+import Button from "./ui/Button";
+import Input from "./forms/Input";
+import Container from "./global/Container";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFacebookF, faGoogle } from "@fortawesome/free-brands-svg-icons";
-import Label from './forms/Label'
 
 const Loginform = () => {
-  return (
-    <Section className="py-10 my-auto">
-    <Container>
-        <div className="flex justify-center -mx-3">
-            <div className="w-full xs:w-4/5 sm:w-3/5 md:w-1/2 lg:w-2/5 xl:w-1/3 px-3">
-                <div className="bg-white dark:bg-slate-950 rounded-lg border border-slate-200 dark:border-slate-800 w-full p-6 pt-5">
-                    <div className="mb-2">
-                        <h3 className="text-xl font-bold text-slate-700 dark:text-white mb-1">
-                            Login
-                        </h3>
-                        <p className="text-sm text-slate-500 dark:text-slate-400">
-                            With valid credentials
-                        </p>
-                    </div>
-                    <div className="py-2">
-                        <Label
-                            htmlFor="emial-address"
-                            className="mb-2"
-                        >
-                            Email Address
-                        </Label>
-                        <Input
-                            placeholder="example@email.com"
-                            id="emial-address"
-                        />
-                    </div>
-                    <div className="py-2">
-                        <Label
-                            htmlFor="password"
-                            className="mb-2 justify-between w-full items-center"
-                        >
-                            Password
-                            <a
-                                className="text-xs text-blue-500 hover:text-blue-700"
-                                href="#"
-                            >
-                                Forgot
-                            </a>
-                        </Label>
-                        <Input
-                            defaultValue="123456"
-                            id="password"
-                            type="password"
-                        />
-                    </div>
-                    <div className="pt-3">
-                        <Button
-                            block
-                            className="bg-blue-600 text-white hover:bg-blue-800"
-                        >
-                            Account Login
-                        </Button>
-                    </div>
-                    <div className="mt-5 mb-4">
-                        <h6 className="text-slate-400 text-[11px] uppercase text-center font-bold tracking-wider">
-                            Login With
-                        </h6>
-                    </div>
-                    <div className="flex flex-wrap -mx-3">
-                        <div className="w-1/2 px-3">
-                            <Button
-                                block
-                                className="bg-white dark:bg-slate-950 text-slate-600 dark:text-slate-200 hover:bg-blue-100 hover:dark:bg-blue-950 hover:text-blue-600 hover:dark:text-blue-600 border border-slate-200 dark:border-slate-800 hover:dark:border-blue-950"
-                            >
-                                <FontAwesomeIcon icon={faGoogle} />
-                                <span>Google</span>
-                            </Button>
-                        </div>
-                        <div className="w-1/2 px-3">
-                            <Button
-                                block
-                                className="bg-white dark:bg-slate-950 text-slate-600 dark:text-slate-200 hover:bg-blue-100 hover:dark:bg-blue-950 hover:text-blue-600 hover:dark:text-blue-600 border border-slate-200 dark:border-slate-800 hover:dark:border-blue-950"
-                            >
-                                <FontAwesomeIcon
-                                    icon={faFacebookF}
-                                />
-                                <span>Facebook</span>
-                            </Button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </Container>
-</Section>
-  )
-}
+  let authForm = useRef();
 
-export default Loginform
+  let [userDetails, setUserDetails] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+
+    // Debugging statements
+    console.log(`Field name: ${name}, Field value: ${value}`);
+
+    // Ensure name is a valid property in userDetails
+    if (name in userDetails) {
+      setUserDetails((prevValue) => ({
+        ...prevValue,
+        [name]: value,
+      }));
+    } else {
+      console.warn(`Unhandled field: ${name}`);
+    }
+  };
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    console.log(userDetails);
+    // Add form submission logic here
+  };
+
+  return (
+    <Container>
+      <div className="flex justify-center items-center -mx-3 py-10">
+        <div className="w-full flex items-center justify-center xs:w-4/5 sm:w-3/5 md:w-1/2 lg:w-2/5 xl:w-1/3 px-3">
+          <section className="w-full bg-white rounded-md flex flex-col px-4 items-center justify-center">
+            <div className="w-full py-3 items-start flex flex-col justify-center">
+              <h1 className="text-lg capitalize font-bold font-sans text-[#334155]">
+                login
+              </h1>
+              <p className="font-semibold text-[13px] text-[#64748B]">
+                With valid credentials
+              </p>
+            </div>
+            <form className="w-full">
+              <Input
+                labelText="email address"
+                inputId="emailAdress"
+                inputName="email"
+                inputType="email"
+                inputPlaceholder="Example@email.com"
+                inputValue="hshshsh"
+                formType='login'
+              />
+              <Input
+                labelText="password"
+                inputId="password"
+                inputName="password"
+                inputType="password"
+                inputPlaceholder="password"
+                inputValue="ddjdjdj"
+                formType='login'
+              />
+              <Button buttonType="submit" buttonContent="account login" />
+              <div className="mt-5 mb-2">
+                <h6 className="text-slate-400 text-[11px] uppercase text-center font-bold tracking-wider">
+                LOGIN WITH
+                </h6>
+              </div>
+              <div className="flex w-full items-center gap-4 py-4 mb-4 justify-between">
+                <button className="w-[45%] flex items-center justify-center gap-3 py-1 outline-none h-[35px] rounded-md border-[1px] border-[#475569]">
+                  <FontAwesomeIcon
+                    icon={faGoogle}
+                    style={{
+                      color: "#475569",
+                      fontSize: "15px",
+                    }}
+                  />
+                  <p>Google</p>
+                </button>
+                <button className="w-[45%] flex items-center justify-center gap-3 py-1 outline-none h-[35px] rounded-md border-[1px] border-[#475569]">
+                  <FontAwesomeIcon
+                    icon={faFacebookF}
+                    style={{
+                      color: "#475569",
+                      fontSize: "15px",
+                    }}
+                  />
+                  <p>Facebook</p>
+                </button>
+              </div>
+            </form>
+          </section>
+        </div>
+      </div>
+    </Container>
+  );
+};
+
+export default Loginform;
--- clientside/src/Components/Registrationform.js
+++ clientside/src/Components/Registrationform.js
@@ -1,116 +1,116 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Button from './ui/Button'
-import Label from './forms/Label'
 import Input from './forms/Input'
-import Section from './global/Section'
 import Container from './global/Container'
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFacebookF, faGoogle } from "@fortawesome/free-brands-svg-icons";
 
 const Registrationform = () => {
+    let [userDetails, setUserDetails] = useState({
+        name: '',
+        email: '',
+        password: ''
+    });
+
+    let handleChange = (e) => {
+        let {name, value} = e.target
+        setUserDetails(prevValue => ({
+            ...prevValue,
+            [name]: value
+        }));
+        console.log(userDetails)
+    }
+
+    const handleAccrountCreation = (e) => {
+        e.preventDefault()
+        console.log(userDetails)
+    }
+
   return (
-    <Section className="py-10 my-auto">
-        <Container>
-            <div className="flex justify-center -mx-3">
-                <div className="w-full xs:w-4/5 sm:w-3/5 md:w-1/2 lg:w-2/5 xl:w-1/3 px-3">
-                    <div className="bg-white dark:bg-slate-950 rounded-lg border border-slate-200 dark:border-slate-800 w-full p-6 pt-5">
-                        <div className="mb-2">
-                            <h3 className="text-xl font-bold text-slate-700 dark:text-white mb-1">
-                                Create Account
-                            </h3>
-                            <p className="text-sm text-slate-500 dark:text-slate-400">
-                                With valid information
-                            </p>
-                        </div>
-                        <div className="py-2">
-                            <Label
-                                htmlFor="full-name"
-                                className="mb-2"
-                            >
-                                Full Name
-                            </Label>
-                            <Input
-                                placeholder="Jhon Doe"
-                                id="full-name"
-                            />
-                        </div>
-                        <div className="py-2">
-                            <Label
-                                htmlFor="emial-address"
-                                className="mb-2"
-                            >
-                                Email Address
-                            </Label>
-                            <Input
-                                placeholder="example@email.com"
-                                id="emial-address"
-                            />
-                        </div>
-                        <div className="py-2">
-                            <Label
-                                htmlFor="password"
-                                className="mb-2 justify-between w-full items-center"
-                            >
-                                Password
-                            </Label>
-                            <Input
-                                id="password"
-                                type="password"
-                            />
-                        </div>
-                        <div className="py-2">
-                            <Label
-                                htmlFor="repeat-password"
-                                className="mb-2 justify-between w-full items-center"
-                            >
-                                Confirm Password
-                            </Label>
-                            <Input
-                                id="repeat-password"
-                                type="password"
-                            />
-                        </div>
-                        <div className="pt-3">
-                            <Button
-                                block
-                                className="bg-blue-600 text-white hover:bg-blue-800"
-                            >
-                                Create Account
-                            </Button>
-                        </div>
-                        <div className="mt-5 mb-4">
-                            <h6 className="text-slate-400 text-[11px] uppercase text-center font-bold tracking-wider">
-                                Sign Up With
-                            </h6>
-                        </div>
-                        <div className="flex flex-wrap -mx-3">
-                            <div className="w-1/2 px-3">
-                                <Button
-                                    block
-                                    className="bg-white dark:bg-slate-950 text-slate-600 dark:text-slate-200 hover:bg-blue-100 hover:dark:bg-blue-950 hover:text-blue-600 hover:dark:text-blue-600 border border-slate-200 dark:border-slate-800 hover:dark:border-blue-950"
-                                >
-                                    <FontAwesomeIcon icon={faGoogle} />
-                                    <span>Google</span>
-                                </Button>
-                            </div>
-                            <div className="w-1/2 px-3">
-                                <Button
-                                    block
-                                    className="bg-white dark:bg-slate-950 text-slate-600 dark:text-slate-200 hover:bg-blue-100 hover:dark:bg-blue-950 hover:text-blue-600 hover:dark:text-blue-600 border border-slate-200 dark:border-slate-800 hover:dark:border-blue-950"
-                                >
-                                    <FontAwesomeIcon
-                                        icon={faFacebookF}
-                                    />
-                                    <span>Facebook</span>
-                                </Button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+    <Container>
+      <div className="flex justify-center items-center -mx-3 py-10">
+        <div className="w-full flex items-center justify-center xs:w-4/5 sm:w-3/5 md:w-1/2 lg:w-2/5 xl:w-1/3 px-3">
+          <section className="w-full bg-white rounded-md flex flex-col px-4 items-center justify-center">
+            <div className="w-full py-3 items-start flex flex-col justify-center">
+              <h1 className="text-lg capitalize font-bold font-sans text-[#334155]">
+              Create Account
+              </h1>
+              <p className="font-semibold text-[13px] text-[#64748B]">
+              With valid information
+              </p>
             </div>
-        </Container>
-    </Section>
+            <form className="w-full">
+            <Input
+                labelText="Full name"
+                inputId="fullName"
+                inputName="name"
+                inputType="text"
+                inputPlaceholder="John Doe"
+                formType='registration'
+                inputOnChange={handleChange}
+                inputValue={userDetails.name}
+              />
+              <Input
+                labelText="email address"
+                inputId="emailAdress"
+                inputName="email"
+                inputType="email"
+                inputPlaceholder="Example@email.com"
+                formType='registration'
+                inputOnChange={handleChange}
+                inputValue={userDetails.email}
+              />
+              <Input
+                labelText="password"
+                inputId="password"
+                inputName="password"
+                inputType="password"
+                formType='registration'
+                inputOnChange={handleChange}
+                inputValue={userDetails.password}
+              />
+            <Input
+                labelText="confirm password"
+                inputId="confirmpPssword"
+                inputName="password"
+                inputType="password"
+                formType='registration'
+                inputOnChange={handleChange}
+              />
+              <Button buttonType="submit" buttonContent="create account" onClick={handleAccrountCreation}/>
+              <div className="mt-5 mb-2">
+                <h6 className="text-slate-400 text-[11px] uppercase text-center font-bold tracking-wider">
+                SIGN UP WITH
+                </h6>
+              </div>
+              <div className="flex w-full items-center gap-4 py-4 mb-4 justify-between">
+                <button className="w-[45%] flex items-center justify-center gap-3 py-1 outline-none h-[35px] rounded-md border-[1px] border-[#475569]">
+                  <FontAwesomeIcon
+                    icon={faGoogle}
+                    style={{
+                      color: "#475569",
+                      fontSize: "15px",
+                    }}
+                  />
+                  <p>Google</p>
+                </button>
+                <button className="w-[45%] flex items-center justify-center gap-3 py-1 outline-none h-[35px] rounded-md border-[1px] border-[#475569]">
+                  <FontAwesomeIcon
+                    icon={faFacebookF}
+                    style={{
+                      color: "#475569",
+                      fontSize: "15px",
+                    }}
+                  />
+                  <p>Facebook</p>
+                </button>
+              </div>
+            </form>
+          </section>
+        </div>
+      </div>
+    </Container>
   )
 }
 
